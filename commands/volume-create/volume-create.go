39c9322ce1eb2a215f5cd3cf23f46a688e42c48d
Kaushal M <kshlmster@gmail.com>
2015-08-03 09:27:30 +0530

    Create dummy volume and store it
    Create a dummy volume, with the given name, when the volume-create api
    is called.
    
    Change-Id: I9909e16f9f95a9ddb41bc7f5f351b6f92c627c5d
    Signed-off-by: Kaushal M <kshlmster@gmail.com>
    Reviewed-on: https://review.gerrithub.io/242026

--- commands/volume-create/volume-create.go
+++ commands/volume-create/volume-create.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/kshlm/glusterd2/context"
 	"github.com/kshlm/glusterd2/rest"
+	"github.com/kshlm/glusterd2/volume"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
@@ -14,11 +16,22 @@ type VolumeCreateCommand struct {
 }
 
 func (c *VolumeCreateCommand) VolumeCreate(w http.ResponseWriter, r *http.Request) {
+	p := mux.Vars(r)
+	volname := p["volname"]
+
 	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "Volume Create API")
 	log.Info("In Volume create API")
+
+	vol := volume.New(volname, "tcp", 2, 3, 4, 5, []string{"brick1", "brick2"})
+	e := context.Store.AddVolume(vol)
+	if e != nil {
+		log.WithField("error", e).Error("Couldn't add volume to store")
+	} else {
+		log.WithField("volume", vol.Name).Debug("NewVolume added to store")
+	}
 }
 
 func (c *VolumeCreateCommand) SetRoutes(router *mux.Router) error {
@@ -27,7 +40,7 @@ func (c *VolumeCreateCommand) SetRoutes(router *mux.Router) error {
 		rest.Route{
 			Name:        "VolumeCreate",
 			Method:      "POST",
-			Pattern:     "/volumes",
+			Pattern:     "/volumes/{volname}",
 			HandlerFunc: c.VolumeCreate},
 	}
 	// Register all routes
