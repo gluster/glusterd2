5281dc3a974bf1e95778ff59fef2b87c4e3a9e7f
Gaurav Kumar Garg <ggarg@redhat.com>
2015-08-18 19:45:55 +0530

    Volfile generation for glusterd2.0 part 1
    This patch will generate client and server volfile. When user create volume from
    REST client it will create volfile in /var/lib/glusterd/vols/<VOLNAME>/*
    directory.
    
    Change-Id: I5dd715b03e80e7db10660aea799152713c1546be

---
+++ volgen/core.go
@@ -0,0 +1,76 @@
+//Core file for graph and volfile generation
+
+package volgen
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kshlm/glusterd2/volume"
+)
+
+const (
+	workdir = "/var/lib/glusterd"
+)
+
+// GenerateVolfile function will do all task from graph generation to volfile generation
+func GenerateVolfile(vinfo *volume.Volinfo) {
+	var cpath string
+	graph := GenerateGraph(vinfo, "CLIENT")
+
+	//Generate client volfile
+	getClientFilePath(vinfo, &cpath)
+	f, err := os.Create(cpath)
+	if err != nil {
+		panic(err)
+	}
+	defer closeFile(f)
+	graph.DumpGraph(f)
+
+	//Generate volfile for server
+	for _, v := range vinfo.Bricks {
+		sgraph := GenerateGraph(vinfo, "SERVER")
+
+		//Generate volfile for server
+		getServerFilePath(vinfo, &cpath, v)
+		f, err := os.Create(cpath)
+		if err != nil {
+			panic(err)
+		}
+		defer closeFile(f)
+		sgraph.DumpGraph(f)
+	}
+}
+
+func getVolumeDir(vinfo *volume.Volinfo, dir *string) {
+	*dir = fmt.Sprintf("%s/vols/%s", workdir, vinfo.Name)
+}
+
+func getServerFilePath(vinfo *volume.Volinfo, path *string, brick string) {
+	var vdir string
+	getVolumeDir(vinfo, &vdir)
+
+	hname, _ := os.Hostname()
+	// Create volume directory (/var/lib/glusterd/vols/<VOLNAME>)
+	err := os.MkdirAll(vdir, 0666)
+	if err != nil {
+		panic(err)
+	}
+	*path = fmt.Sprintf("%s/%s.%s.%s.vol", vdir, vinfo.Name, hname, brick)
+}
+
+func getClientFilePath(vinfo *volume.Volinfo, path *string) {
+	var vdir string
+	getVolumeDir(vinfo, &vdir)
+
+	// Create volume directory (/var/lib/glusterd/vols/<VOLNAME>)
+	err := os.MkdirAll(vdir, 0666)
+	if err != nil {
+		panic(err)
+	}
+	*path = fmt.Sprintf("%s/trusted-%s.tcp-fuse.vol", vdir, vinfo.Name)
+}
+
+func closeFile(f *os.File) {
+	f.Close()
+}
