79e2cdd41c80d60c4e0f4b52b8b0462c8b90dd21
Atin Mukherjee <atin.mukherjee83@gmail.com>
2015-08-20 13:53:51 +0530

    response : define generic json response structure for rest of the commands
    Closes #11
    
    Change-Id: I961895830b10ff459d8e70868369afb14aaf89d0
    Signed-off-by: Atin Mukherjee <atin.mukherjee83@gmail.com>

--- errors/error.go
+++ errors/error.go
@@ -6,5 +6,11 @@ import (
 
 // Different error macros
 var (
-	ErrVolNotFound = errors.New("Volume not found")
+	ErrVolNotFound       = errors.New("Volume not found")
+	ErrJSONParsingFailed = errors.New("Unable to parse the request")
+	ErrEmptyVolName      = errors.New("Volume name is empty")
+	ErrEmptyBrickList    = errors.New("Brick list is empty")
+	ErrVolExists         = errors.New("Volume already exists")
+	ErrVolAlreadyStarted = errors.New("Volume already started")
+	ErrVolAlreadyStopped = errors.New("Volume already stopped")
 )
