a45bc6991a3d80cbdb512d4a68a5a23f3cf876dc
Atin Mukherjee <atin.mukherjee83@gmail.com>
2015-08-20 17:41:02 +0530

    apis: add add-brick & remove-brick apis
    Change-Id: I42b89063267105abbd213260467e36ae75540b38
    Signed-off-by: Atin Mukherjee <atin.mukherjee83@gmail.com>

---
+++ commands/volume-remove-brick/volume-remove-brick.go
@@ -0,0 +1,73 @@
+// Package volumeremovebrick implements the volume remove-brick command for GlusterD
+package volumeremovebrick
+
+import (
+	"net/http"
+
+	"github.com/kshlm/glusterd2/client"
+	"github.com/kshlm/glusterd2/context"
+	"github.com/kshlm/glusterd2/errors"
+	"github.com/kshlm/glusterd2/rest"
+	"github.com/kshlm/glusterd2/utils"
+	"github.com/kshlm/glusterd2/volume"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// Command is a holding struct used to implement the GlusterD Command interface
+// for the volume remove-brick command
+type Command struct {
+}
+
+func (c *Command) volumeRemoveBrick(w http.ResponseWriter, r *http.Request) {
+
+	var msg volume.VolRemoveBrickRequest
+	var vol *volume.Volinfo
+
+	e := utils.GetJSONFromRequest(r, &msg)
+	if e != nil {
+		client.SendResponse(w, -1, 422, errors.ErrJSONParsingFailed.Error(), 422, "")
+		return
+	}
+
+	if msg.Name == "" {
+		log.Error("Volume name is empty")
+		client.SendResponse(w, -1, http.StatusBadRequest, errors.ErrEmptyVolName.Error(), http.StatusBadRequest, "")
+		return
+	}
+	if len(msg.Bricks) <= 0 {
+		log.Error("Brick list is empty")
+		client.SendResponse(w, -1, http.StatusBadRequest, errors.ErrEmptyBrickList.Error(), http.StatusBadRequest, "")
+		return
+	}
+
+	vol, e = context.Store.GetVolume(msg.Name)
+	if e != nil {
+		log.WithField("Volume", msg.Name).Error("Volume doesn't exist")
+		client.SendResponse(w, -1, http.StatusBadRequest, errors.ErrVolNotFound.Error(), http.StatusBadRequest, "")
+		return
+	}
+	//TODO : Add logic to match the existing bricks and then remove
+
+	e = context.Store.AddOrUpdateVolume(vol)
+	if e != nil {
+		log.WithField("error", e).Error("Couldn't update volume to store")
+		client.SendResponse(w, -1, http.StatusInternalServerError, e.Error(), http.StatusInternalServerError, "")
+		return
+	}
+
+	log.WithField("volume", vol.Name).Debug("Volume updated into store")
+	client.SendResponse(w, 0, 0, "", http.StatusOK, "")
+}
+
+// Routes returns command routes to be set up for the volume remove-brick command.
+func (c *Command) Routes() rest.Routes {
+	return rest.Routes{
+		// VolumeRemoveBrick
+		rest.Route{
+			Name:        "VolumeRemoveBrick",
+			Method:      "POST",
+			Pattern:     "/volumes/remove-brick",
+			HandlerFunc: c.volumeRemoveBrick},
+	}
+}
