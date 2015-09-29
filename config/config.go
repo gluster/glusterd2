e254f2b2549414ab6a3b68ff1f406efb8c00a837
Kaushal M <kshlmster@gmail.com>
2015-09-29 21:15:09 +0530

    Add config changes for local state directory
    ...forgot to add these changes in the previous commit.

--- config/config.go
+++ config/config.go
@@ -6,14 +6,28 @@ package config
 
 import (
 	"flag"
+	"os"
+	"path"
 )
 
 // All configuration values which should be available for use by other packages need to be defined here as global variables.
+// Any configuration that doesn't require custom default values can have their flags initialised right here.
+// Any configuration that require custom default values should be initalized manually.
 var (
 	LogLevel    = flag.String("loglevel", "debug", "Log messages upto this level")
 	RestAddress = flag.String("restaddress", ":24007", "Address to bind REST endpoint to")
+
+	LocalStateDir string
 )
 
+func initLocalStateDir() {
+	wd, _ := os.Getwd()
+
+	flag.StringVar(&LocalStateDir, "localstatedir", path.Join(wd, "glusterd"), "Directory to store local state information.")
+}
+
 func init() {
+	initLocalStateDir()
+
 	flag.Parse()
 }
