aef459589367751f5a6b7bbd24e01579239ce49c
Atin Mukherjee <amukherj@redhat.com>
2015-11-23 11:52:20 +0530

    change commands/doc.go
    Signed-off-by: Atin Mukherjee <amukherj@redhat.com>

--- commands/doc.go
+++ commands/doc.go
@@ -8,14 +8,16 @@ of registered routes and calls a handler for the route that matches the URL or
 other conditions.
 
 Route models a route to be set on the GlusterD Rest server and holds the name,
-pattern and the registered handler function. Individual commands should define
-its route table and glusterd while initializing will iterate over all these
-router tables and register them. The handler function is the one which holds the
-logic of how the command is going to be executed.
+pattern and the registered handler function. Group of mgmt commands like peers,
+volumes  should define its route table and glusterd while initializing will
+iterate over all these router tables and register them. The handler function is
+the one which holds the logic of how the command is going to be executed.
 
-Developers are expected to create individual directories for every commands
-under command folder in the code repository and put up a .go file with the same
-command name. Along with that individual commands must have an entry in the
-Commands interface table in command.go.
+Developers are expected to create directories for each group of commands
+under command folder in the codebase. Say for all peer related commands,
+commands/peers directory should exist. Inside commands/peers, a commands.go file
+should define all the ReST router details and respective handlers should be
+defined in inidividual .go file for every commands. dAlong with that individual
+commands must have an entry in the
 */
 package commands
