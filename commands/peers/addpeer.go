21bd35e094ac998a155c0e5d9be6232f3ce87412
Gaurav Kumar Garg <garg.gaurav52@gmail.com>
2016-01-21 15:46:06 +0530

    etcd bootstrapping in glusterd 2.0
    Adding member upon peer probe in the etcd cluster.
    
    With this change it will add remote host url during peer probe of remote host
    in the cluster.
    
    Issue #72
    
    Signed-off-by: Gaurav Kumar Garg <garg.gaurav52@gmail.com>

--- commands/peers/addpeer.go
+++ commands/peers/addpeer.go
@@ -1,13 +1,19 @@
 package peercommands
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gluster/glusterd2/errors"
 	"github.com/gluster/glusterd2/peer"
 	"github.com/gluster/glusterd2/rest"
 	"github.com/gluster/glusterd2/rpc/client"
 	"github.com/gluster/glusterd2/utils"
+
+	log "github.com/Sirupsen/logrus"
+	etcdclient "github.com/coreos/etcd/client"
+	etcdcontext "golang.org/x/net/context"
 
 	"github.com/pborman/uuid"
 )
@@ -43,6 +49,44 @@ func addPeerHandler(w http.ResponseWriter, r *http.Request) {
 		rest.SendHTTPError(w, http.StatusInternalServerError, *rsp.OpError)
 		return
 	}
+
+	// Add member to etcd server
+	var c etcdclient.Client
+	mAPI := etcdclient.NewMembersAPI(c)
+	//_, e = membersAPI.Add(context.Background(), p.Name)
+	member, e := mAPI.Add(etcdcontext.Background(), p.Name)
+	if e != nil {
+		//rest.SendHTTPError(w, http.StatusInternalServerError, strings.e)
+		rest.SendHTTPError(w, http.StatusInternalServerError, e.Error())
+		return
+	}
+
+	newID := member.ID
+	newName := member.Name
+
+	log.Info("New member named %s with ID %s added to cluster", newName, newID)
+
+	mlist, e := mAPI.List(etcdcontext.Background())
+	if e != nil {
+		rest.SendHTTPError(w, http.StatusInternalServerError, e.Error())
+		return
+	}
+
+	conf := []string{}
+	for _, memb := range mlist {
+		for _, u := range memb.PeerURLs {
+			n := memb.Name
+			if memb.ID == newID {
+				n = newName
+			}
+			conf = append(conf, fmt.Sprintf("%s=%s", n, u))
+		}
+	}
+
+	log.Info("ETCD_NAME=%q", newName)
+	log.Info("ETCD_INITIAL_CLUSTER=%q", strings.Join(conf, ","))
+	log.Info("ETCD_INITIAL_CLUSTER_STATE=\"existing\"")
+
 	if e = peer.AddOrUpdatePeer(p); e != nil {
 		rest.SendHTTPError(w, http.StatusInternalServerError, e.Error())
 		return
