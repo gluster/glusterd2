bd7baf651d06b45eb53d087306c3d5f3040fe47f
Gaurav Kumar Garg <garg.gaurav52@gmail.com>
2016-01-30 12:22:01 +0530

    Do not kill etcd daemon upon issuing ^c (SIGINT) in glusterd daemon
    Currently when etcd daemon start upon starting of glusterd2 then upon
    performing ^C (SIGINT) its killing etcd daemon.
    
    With this patch it will set child process id to different from main
    process id group and, etcd daemon will not be kill upon performing
    SIGINT (^C) of main process.
    
    Fixing some minor issue in unit text case
    
    Issue #66
    
    Signed-off-by: Gaurav Kumar Garg <garg.gaurav52@gmail.com>
    Signed-off-by : Atin Mukherjee <amukherj@redhat.com>

--- etcdmgmt/etcd-mgmt_test.go
+++ etcdmgmt/etcd-mgmt_test.go
@@ -1,7 +1,6 @@
 package etcdmgmt
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/gluster/glusterd2/tests"
@@ -12,7 +11,6 @@ func TestStartETCDWithInvalidExecName(t *testing.T) {
 	// Mock the executable name such that it fails
 	defer heketitests.Patch(&ExecName, "abc").Restore()
 	_, err := StartETCD()
-	fmt.Println("error is: ", err)
 	tests.Assert(t, err != nil)
 }
 
