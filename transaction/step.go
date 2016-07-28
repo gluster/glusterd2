ca4918bc75c6210ba8487d6d7630f59449f82862
Kaushal M <kshlmster@gmail.com>
2016-07-28 15:59:24 +0530

    TMP: Begin runStepFuncOnNode implementation

--- transaction/step.go
+++ transaction/step.go
@@ -2,10 +2,15 @@ package transaction
 
 import (
 	"errors"
+	"net"
+	"net/rpc"
 
 	"github.com/gluster/glusterd2/context"
+	"github.com/gluster/glusterd2/peer"
 
+	"github.com/kshlm/pbrpc/pbcodec"
 	"github.com/pborman/uuid"
+	config "github.com/spf13/viper"
 )
 
 // StepFunc is the function that is supposed to be run during a transaction step
@@ -31,33 +36,80 @@ type Step struct {
 	Nodes    []uuid.UUID
 }
 
+var (
+	ErrStepFuncNotFound = errors.New("StepFunc was not found")
+)
+
 // do runs the DoFunc on the nodes
 func (s *Step) do(c *context.Context) error {
-	for range s.Nodes {
-		// RunStepFunconNode(s.DoFunc, n)
-	}
-	c.Log.WithField("step", s.DoFunc).Debug("running step")
-
-	doFunc, ok := GetStepFunc(s.DoFunc)
-	if !ok {
-		return errors.New("StepFunc was not found")
-	}
-	return doFunc(c)
+	return runStepFuncOnNodes(s.DoFunc, c, s.Nodes)
 }
 
 // undo runs the UndoFunc on the nodes
 func (s *Step) undo(c *context.Context) error {
 	if s.UndoFunc != "" {
-		for range s.Nodes {
-			// RunStepFunconNode(s.UndoFunc, n)
-		}
-		c.Log.WithField("undostep", s.UndoFunc).Debug("running undostep")
-
-		undoFunc, ok := GetStepFunc(s.UndoFunc)
-		if !ok {
-			return errors.New("StepFunc was not found")
-		}
-		return undoFunc(c)
+		return runStepFuncOnNodes(s.UndoFunc, c, s.Nodes)
 	}
 	return nil
+}
+
+func runStepFuncOnNodes(name string, c *context.Context, nodes []uuid.UUID) error {
+	done := make(chan error)
+	defer close(done)
+
+	for i, node := range nodes {
+		go runStepFuncOnNode(name, c, node, done)
+	}
+
+	// TODO: Need to properly aggregate results
+	var err error
+	for i >= 0 {
+		err <- done
+		i--
+	}
+	return err
+}
+
+func runStepFuncOnNode(name string, c *context.Context, node uuid.UUID, done chan<- error) {
+	if node == context.MyUUID {
+		done <- runStepFuncLocal(name, c)
+	} else {
+		done <- runStepFuncRemote(name, c, node)
+	}
+}
+
+func runStepFuncLocal(name string, c *context.Context) error {
+	c.Log.WithField("stepfunc", name).Debug("running step function")
+
+	stepFunc, ok := GetStepFunc(name)
+	if !ok {
+		return ErrStepFuncNotFound
+	}
+	return stepFunc(c)
+}
+
+func runStepFuncRemote(name string, c *context.Context, node uuid.UUID) error {
+	// TODO: I'm creating connections on demand. This should be changed so that
+	// we have long term connections.
+	p, err := peer.GetPeer(node.String())
+	if err != nil {
+		return err
+	}
+
+	var conn net.Conn
+	port := config.GetString("rpcport")
+
+	for _, addr := range p.Addresses {
+		conn, err = net.Dial("tcp", addr+":"+port)
+		if err == nil && conn != nil {
+			break
+		}
+	}
+	if conn == nil {
+		return err
+	}
+
+	client := rpc.NewClientWithCodec(pbcodec.NewClientCodec(conn))
+	defer client.Close()
+
 }
