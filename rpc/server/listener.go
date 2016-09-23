de34ba2743b85a3156740200110bdb11c41357de
Kaushal M <kshlmster@gmail.com>
2016-09-23 14:02:51 +0530

    rpc/server: Switch to gRPC
    Actual services will be switched in a subsequent patch.

--- rpc/server/listener.go
+++ rpc/server/listener.go
@@ -2,19 +2,15 @@ package server
 
 import (
 	"net"
-	"net/rpc"
-
-	"github.com/gluster/glusterd2/rpc/services"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/kshlm/pbrpc/pbcodec"
 	config "github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 // StartListener is to register all the services and start listening on them
 func StartListener() error {
-	server := rpc.NewServer()
-	services.RegisterServices(server)
+	server := grpc.NewServer()
 
 	listenAddr := config.GetString("rpcaddress")
 
@@ -26,16 +22,6 @@ func StartListener() error {
 		log.WithField("ip:port", listenAddr).Info("Registered RPC Listener")
 	}
 
-	go func() {
-		for {
-			c, e := l.Accept()
-			if e != nil {
-				log.WithField("error", e).Info("Accept failed")
-				continue
-			}
-			log.WithField("Connection", c.RemoteAddr()).Info("New incoming connection")
-			go server.ServeCodec(pbcodec.NewServerCodec(c))
-		}
-	}()
+	server.Serve(l)
 	return nil
 }
---
+++ rpc/server/services.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"google.golang.org/grpc"
+)
+
+// Service is the interface that packages that want to register with the GD2
+// RPC server need to implement
+type Service interface {
+	// RegisterService should register available gRPC Services with the given Server
+	// For eg.
+	// 	type SomeSvc int
+	// 	func (svc *SomeSvc) RegisterServive(s *grpc.Server) {
+	// 		RegisterSomeSvcServer(s, svc)
+	// 	}
+	RegisterService(s *grpc.Server)
+}
+
+var services []Service
+
+// Register adds the provided Service to the GD2 server services list, which is
+// then registered with the grpc.Server.
+// Packages need Register during their initialization, to allow all services to
+// be registered before StartListener is called.
+func Register(svc Service) {
+	services = append(services, svc)
+}
+
+// registerServices is called by StartListener to get the services list
+// registered with the grpc.Server
+func registerServices(s *grpc.Server) {
+	for _, svc := range services {
+		svc.RegisterService(s)
+	}
+}
