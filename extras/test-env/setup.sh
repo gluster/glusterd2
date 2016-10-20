eb942165bdf1e2db0d2b82df96e0e186223212ac
Kaushal M <kshlmster@gmail.com>
2016-10-20 13:53:51 +0530

    extras/test-env: setup script should cleanup temp dir

--- extras/test-env/setup.sh
+++ extras/test-env/setup.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 set -x -e
-TMP=$(mktemp -d)
+TMPDIR=$(mktemp -d)
 
 # GD2_VERSION will be set as an ENV variable form the Dockerfile
 ARCHIVE=glusterd2-${GD2_VERSION}-linux-amd64.tar.xz
 URL=https://github.com/gluster/glusterd2/releases/download/${GD2_VERSION}/${ARCHIVE}
 
-curl -o ${TMP}/${ARCHIVE} -L $URL
-tar -C /usr/sbin --xz -xf ${TMP}/${ARCHIVE}
+curl -o ${TMPDIR}/${ARCHIVE} -L $URL
+tar -C /usr/sbin --xz -xf ${TMPDIR}/${ARCHIVE}
 setcap cap_sys_admin+ep /usr/sbin/glusterd2
 
 yum install -y etcd
+
 yum clean all
+rm -rf $TMPDIR
 
 
 
