e2d8fec4b2ce101ae5a58fb5783e0eab6ec507fa
Kaushal M <kshlmster@gmail.com>
2017-02-27 20:25:52 +0530

    Directly use GOTAGS with `go build`

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,12 +14,10 @@ VERSION=$($(dirname $0)/pkg-version --full)
 LDFLAGS="-X github.com/gluster/glusterd2/gdctx.GlusterdVersion=$VERSION"
 BIN=$(basename $(go list -f '{{.ImportPath}}'))
 
-## This so that mgmt builds without the libvirt and augeas resrouces on centos-ci
-## TODO Remove this once mgmt can be built engine only without resources
-export GOTAGS='novirt noaugeas'
-
 echo "Building $BIN $VERSION"
-go build -ldflags "${LDFLAGS}" -o $OUTDIR/$BIN || exit 1
+## Adding GOTAGS so that mgmt builds without the libvirt and augeas resrouces on centos-ci
+## TODO Remove this once mgmt can be built engine only without resources
+GOTAGS='novirt noaugeas' go build -ldflags "${LDFLAGS}" -o $OUTDIR/$BIN || exit 1
 echo "Built $BIN $VERSION at $OUTDIR/$BIN"
 
 
