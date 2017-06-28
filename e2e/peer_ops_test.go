86590e10972cd15b37c89c0e15087ded45d32959
Prashanth Pai <ppai@redhat.com>
2017-06-28 18:16:11 +0530

    Fix panics during functest
    I assumed assert() will exit the test, but it's require() that exits
    the test being run when the condition being tested is false.
    
    Signed-off-by: Prashanth Pai <ppai@redhat.com>

--- e2e/peer_ops_test.go
+++ e2e/peer_ops_test.go
@@ -6,37 +6,37 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAddRemovePeer(t *testing.T) {
-	assert := assert.New(t)
+	r := require.New(t)
 
 	g1, err := spawnGlusterd("./config/1.yaml")
-	assert.Nil(err)
+	r.Nil(err)
 	defer g1.Stop()
 	defer g1.EraseWorkdir()
-	assert.True(g1.IsRunning())
+	r.True(g1.IsRunning())
 
 	g2, err := spawnGlusterd("./config/2.yaml")
-	assert.Nil(err)
+	r.Nil(err)
 	defer g2.Stop()
 	defer g2.EraseWorkdir()
-	assert.True(g2.IsRunning())
+	r.True(g2.IsRunning())
 
 	// add peer: ask g1 to add g2 as peer
 	reqBody := strings.NewReader(fmt.Sprintf(`{"addresses": ["%s"]}`, g2.PeerAddress))
 	resp, err := http.Post("http://"+g1.ClientAddress+"/v1/peers", "application/json", reqBody)
-	assert.Nil(err)
+	r.Nil(err)
 	defer resp.Body.Close()
-	assert.Equal(resp.StatusCode, 201)
+	r.Equal(resp.StatusCode, 201)
 
 	// remove peer: ask g1 to remove g2 as peer
 	delURL := fmt.Sprintf("http://%s/v1/peers/%s", g1.ClientAddress, g2.PeerID())
 	req, err := http.NewRequest("DELETE", delURL, nil)
-	assert.Nil(err)
+	r.Nil(err)
 	resp, err = http.DefaultClient.Do(req)
-	assert.Nil(err)
+	r.Nil(err)
 	defer resp.Body.Close()
-	assert.Equal(resp.StatusCode, 204)
+	r.Equal(resp.StatusCode, 204)
 }
