68caadba760b03bf22ef807e284127535b573ec5
Kaushal M <kshlmster@gmail.com>
2017-07-03 16:49:42 +0530

    Add a build-id to built binary

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,6 +13,7 @@ fi
 GOBUILD_TAGS="novirt noaugeas "
 VERSION=$($(dirname $0)/pkg-version --full)
 LDFLAGS="-X github.com/gluster/glusterd2/gdctx.GlusterdVersion=$VERSION"
+LDFLAGS+=" -B 0x$(head -c20 /dev/urandom | od -An -tx1 | tr -d ' \n')"
 BIN=$(basename $(go list -f '{{.ImportPath}}'))
 
 if [ "$PLUGINS" == "yes" ]; then
