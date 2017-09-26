079ff07a6150eb346977431a59f5ed7d1c49b315
Prashanth Pai <ppai@redhat.com>
2017-09-26 15:43:01 +0530

    log: Associate reqid with client request logs
    Fixes #344
    
    Signed-off-by: Prashanth Pai <ppai@redhat.com>

--- middleware/request_logging.go
+++ middleware/request_logging.go
@@ -10,5 +10,8 @@ import (
 // LogRequest is a middleware which logs HTTP requests in the
 // Apache Common Log Format (CLF)
 func LogRequest(next http.Handler) http.Handler {
-	return handlers.LoggingHandler(log.StandardLogger().Writer(), next)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		l := log.WithField("reqid", r.Header.Get("X-Request-ID"))
+		handlers.LoggingHandler(l.Writer(), next).ServeHTTP(w, r)
+	})
 }
