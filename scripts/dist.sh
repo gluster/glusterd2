2042257c66396a9037f7ae789a7c4ae4cc6167ee
Kaushal M <kshlmster@gmail.com>
2017-11-09 13:37:34 +0530

    Use git-archive to create dist archives

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -x
 # This script builds a dist tarball of the GD2 source
 # This should only be called from the root of the GD2 repo
 
@@ -8,19 +8,16 @@ OUTDIR=${1:-.}
 
 VERSION=$($(dirname $0)/pkg-version --full)
 
-TARNAME=glusterd2-$VERSION
+BASENAME=glusterd2-$VERSION
+TARNAME=$BASENAME
 case $VENDOR in
   yes|y|Y)
     TARNAME+="-vendor"
     ;;
-
-  *)
-    EXCLUDEVENDOR="--exclude vendor"
-    ;;
 esac
 
-TARBALL=$TARNAME.tar
-ARCHIVE=$OUTDIR/$TARBALL.xz
+TARFILE=$OUTDIR/$TARNAME.tar
+ARCHIVE=$TARFILE.xz
 SIGNFILE=$ARCHIVE.asc
 
 # Cleanup old archives
@@ -37,17 +34,22 @@ $(dirname $0)/gen-version.sh
 # Use a temp dir to build tar file
 TMPDIR=$(mktemp -d)
 
-echo "Creating dist tarball $ARCHIVE"
-# Exclude .git, releases, builds and older tarballs
-tar --exclude .git --exclude releases --exclude build --exclude 'glusterd2-*.tar.xz*' $EXCLUDEVENDOR --transform "s/^\./$TARNAME/" -cf $TMPDIR/$TARBALL . || exit 1
-xz --stdout $TMPDIR/$TARBALL > $ARCHIVE || exit 1
-echo "Created dist tarball $ARCHIVE"
+echo "Creating dist archive $ARCHIVE"
+git archive -o $TARFILE --prefix "$BASENAME/" HEAD
+case $VENDOR in
+  yes|y|Y)
+    tar --transform "s/^\./$BASENAME/" -rf $TARFILE ./vendor || exit 1
+    ;;
+esac
+
+xz $TARFILE || exit 1
+echo "Created dist archive $ARCHIVE"
 
 
 # Sign the generated archive
-echo "Signing dist tarball"
+echo "Signing dist archive"
 gpg --armor --output $SIGNFILE --detach-sign $ARCHIVE || exit 1
-echo "Signed dist tarball, signature in $SIGNFILE"
+echo "Signed dist archive, signature in $SIGNFILE"
 
 
 # Remove the VERSION file, it is no longer needed, and TMPDIR
