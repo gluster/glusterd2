369c3ba8c38b39438d3c8604aa5fbe7ddcfa5b8c
Aravinda VK <mail@aravindavk.in>
2017-11-09 10:02:47 +0530

    restapi: Fixed Auth test
    Updates: #252
    Signed-off-by: Aravinda VK <avishwan@redhat.com>

--- e2e/restapi_auth_test.go
+++ e2e/restapi_auth_test.go
@@ -3,7 +3,6 @@ package e2e
 import (
 	"io/ioutil"
 	"testing"
-	"time"
 
 	"github.com/gluster/glusterd2/pkg/restclient"
 
@@ -13,15 +12,11 @@ import (
 func TestRESTAPIAuth(t *testing.T) {
 	r := require.New(t)
 
-	// FIXME: Ignored error since it makes REST call
-	g1, _ := spawnGlusterd("./config/4.yaml", true)
-
+	g1, err := spawnGlusterd("./config/4.yaml", true)
+	r.Nil(err)
 	defer g1.Stop()
 	defer g1.EraseWorkdir()
 	r.True(g1.IsRunning())
-
-	// Sleep till Glusterd spawns and generates Auth
-	time.Sleep(5 * time.Second)
 
 	secret, err := ioutil.ReadFile(g1.Workdir + "/auth")
 	r.Nil(err)
