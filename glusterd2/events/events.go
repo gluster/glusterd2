3222dbaddcd30a94c906b24f1edd639a0cef3eb0
Prashanth Pai <ppai@redhat.com>
2018-01-30 17:03:51 +0530

    Log events to a separate log file (in JSON)
    Signed-off-by: Prashanth Pai <ppai@redhat.com>

--- glusterd2/events/events.go
+++ glusterd2/events/events.go
@@ -12,19 +12,19 @@ import (
 // Event represents an event in GD2
 type Event struct {
 	// ID is a unique event ID
-	ID uuid.UUID
+	ID uuid.UUID `json:"id"`
 	// Name is the the name of the event
-	Name string
+	Name string `json:"name"`
 	// Data is any additional data attached to the event.
-	Data map[string]string
+	Data map[string]string `json:"data,omitempty"`
 	// global should be set to true to broadcast event to the full GD2 cluster.
 	// If not event is only broadcast in the local node
 	global bool
 	// Origin is used when broadcasting global events to prevent origin nodes
 	// rebroadcasting a global event. Event generators need not set this.
-	Origin uuid.UUID
+	Origin uuid.UUID `json:"origin"`
 	// Timestamp is the time when the event was created
-	Timestamp time.Time
+	Timestamp time.Time `json:"timestamp"`
 }
 
 // New returns a new Event with given information
