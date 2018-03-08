9c5d02d8589bd15691318d56ef000bdb901a8f40
Aravinda VK <avishwan@redhat.com>
2018-03-08 13:39:50 +0530

    cli: Support for external sub commands
    Added support for external sub commands to Gluster CLI.(Similar to
    `git` external sub command support)
    
    If any executable available in `PATH` with name `glustercli-<cmd>` then
    it will be available as `glustercli <cmd>`. External sub command can be
    written in any programming language.
    
    All the known commands will get precedence over external commands.
    
    Following arguments will be included while executing external commands.
    
        --xml
        --json
        --glusterd-host <host>
        --glusterd-https
        --glusterd-port <port>
        --logdir <path>
        --logfile <name>
        --loglevel <level>
        --cacert <path>
        --insecure
    
    Example of simple external plugin/command, which connects to Glusterd
    and just displays Volume name and State. (File name
    `/usr/local/bin/glustercli-volinfo2` with executable permission)
    
        #!/usr/bin/env python
        from argparse import ArgumentParser
        import sys
        import json
    
        import requests
    
        if __name__ == "__main__":
            parser = ArgumentParser()
            parser.add_argument("--xml", action="store_true")
            parser.add_argument("--json", action="store_true")
            parser.add_argument("--glusterd-host", default="localhost")
            parser.add_argument("--glusterd-https", action="store_true")
            parser.add_argument("--glusterd-port", default=24007, type=int)
            parser.add_argument("--logdir")
            parser.add_argument("--logfile")
            parser.add_argument("--loglevel")
            parser.add_argument("--cacert")
            parser.add_argument("--insecure", action="store_true")
    
            parser.add_argument("--volume")
    
            args = parser.parse_args()
            url = "https://" if args.glusterd_https else "http://"
            url += "%s:%d/v1/volumes" % (args.glusterd_host, args.glusterd_port)
            if args.volume:
                url += "/" + args.volume
    
            resp = requests.get(url)
    
            if resp.status_code != 200:
                print("Unable to get Volume Info")
                sys.exit(1)
    
            for v in json.loads(resp.content):
                print("%15s %s" % (v["name"], v["state"]))
    
    Fixes: #594
    Signed-off-by: Aravinda VK <avishwan@redhat.com>

--- glustercli/main.go
+++ glustercli/main.go
@@ -3,16 +3,79 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"regexp"
+	"syscall"
 
 	"github.com/gluster/glusterd2/glustercli/cmd"
+	"github.com/gluster/glusterd2/pkg/logging"
 )
+
+var unknownCommandExp = regexp.MustCompile(`unknown command "([^"]+)" `)
+
+func getUnknownCommandName(err error) string {
+	m := unknownCommandExp.FindStringSubmatch(err.Error())
+	if len(m) == 2 {
+		return m[1]
+	}
+	return ""
+}
 
 func main() {
 	// Migrate old format Args into new Format. Modifies os.Args[]
 	argsMigrate()
 
+	cmd.RootCmd.SilenceErrors = true
+
 	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		unknownCmd := getUnknownCommandName(err)
+		if unknownCmd != "" {
+			// May be external sub Command
+			cmdPath, err := exec.LookPath(cmd.RootCmd.Use + "-" + unknownCmd)
+			if err == nil {
+				cmdArgs := []string{}
+				err = cmd.RootCmd.ParseFlags(os.Args[1:])
+				if err == nil {
+					if cmd.FlagXMLOutput {
+						cmdArgs = append(cmdArgs, "--xml")
+					}
+
+					if cmd.FlagJSONOutput {
+						cmdArgs = append(cmdArgs, "--json")
+					}
+					cmdArgs = append(cmdArgs, "--glusterd-host", cmd.FlagHostname)
+					if cmd.FlagHTTPS {
+						cmdArgs = append(cmdArgs, "--glusterd-https")
+					}
+					cmdArgs = append(cmdArgs, "--glusterd-port", fmt.Sprintf("%d", cmd.FlagPort))
+					cmdArgs = append(cmdArgs, "--"+logging.DirFlag, cmd.FlagLogDir)
+					cmdArgs = append(cmdArgs, "--"+logging.FileFlag, cmd.FlagLogFile)
+					cmdArgs = append(cmdArgs, "--"+logging.LevelFlag, cmd.FlagLogLevel)
+					cmdArgs = append(cmdArgs, "--cacert", cmd.FlagCacert)
+					if cmd.FlagInsecure {
+						cmdArgs = append(cmdArgs, "--insecure")
+					}
+				}
+
+				command := exec.Command(cmdPath, cmdArgs...)
+				command.Stdout = os.Stdout
+				command.Stderr = os.Stderr
+
+				// If Return code is accessible
+				if err = command.Run(); err != nil {
+					if exiterr, ok := err.(*exec.ExitError); ok {
+						if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+							os.Exit(status.ExitStatus())
+						}
+					}
+					os.Exit(1)
+				}
+				return
+			}
+		}
+
+		// If Known command error or no external command available
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 }
