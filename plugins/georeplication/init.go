e23e718fb3f7bafae76ab7151082e2bf04870f3a
Aravinda VK <avishwan@redhat.com>
2018-04-23 10:22:55 +0530

    plugins: Remove SunRPC pluggability
    None of the plugins exposed sunrpc programs using plugin interface.
    
    Removing unused part from the Plugin interface.
    
    Signed-off-by: Aravinda VK <avishwan@redhat.com>

--- plugins/georeplication/init.go
+++ plugins/georeplication/init.go
@@ -3,7 +3,6 @@ package georeplication
 import (
 	"github.com/gluster/glusterd2/glusterd2/servers/rest/route"
 	"github.com/gluster/glusterd2/glusterd2/transaction"
-	"github.com/gluster/glusterd2/pkg/sunrpc"
 )
 
 // Plugin is a structure which implements GlusterdPlugin interface
@@ -13,11 +12,6 @@ type Plugin struct {
 // Name returns name of plugin
 func (p *Plugin) Name() string {
 	return "georeplication"
-}
-
-// SunRPCProgram returns sunrpc program to register with Glusterd
-func (p *Plugin) SunRPCProgram() sunrpc.Program {
-	return nil
 }
 
 // RestRoutes returns list of REST API routes to register with Glusterd
