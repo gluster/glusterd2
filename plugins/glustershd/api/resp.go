3bb831e229faa5b4c75d55a3beec8febbf5788a7
root <root@dhcp41-154.lab.eng.blr.redhat.com>
2018-06-08 13:39:08 +0530

    Add missing files gfid field in self heal info API as well as cli.
    Signed-off-by: Vishal Pandey <vpandey@redhat.com>

--- plugins/glustershd/api/resp.go
+++ plugins/glustershd/api/resp.go
@@ -4,16 +4,23 @@ import (
 	"encoding/xml"
 )
 
+// FileGfID represents the file details on a volume
+type FileGfID struct {
+	Filename string `xml:",chardata"`
+	GfID     string `xml:"gfid,attr"`
+}
+
 // BrickHealInfo represents brick details for Heal Info command
 type BrickHealInfo struct {
-	HostID                 *string `xml:"hostUuid,attr" json:"host-id"`
-	Name                   *string `xml:"name" json:"name"`
-	Status                 *string `xml:"status" json:"status"`
-	TotalEntries           *int    `xml:"totalNumberOfEntries" json:"total-entries,omitempty"`
-	EntriesInHealPending   *int    `xml:"numberOfEntriesInHealPending" json:"entries-in-heal-pending,omitempty"`
-	EntriesInSplitBrain    *int    `xml:"numberOfEntriesInSplitBrain" json:"entries-in-split-brain,omitempty"`
-	EntriesPossiblyHealing *int    `xml:"numberOfEntriesPossiblyHealing" json:"entries-possibly-healing,omitempty"`
-	Entries                *int    `xml:"numberOfEntries" json:"entries,omitempty"`
+	HostID                 string     `xml:"hostUuid,attr" json:"host-id"`
+	Name                   string     `xml:"name" json:"name"`
+	Status                 string     `xml:"status" json:"status"`
+	TotalEntries           *int       `xml:"totalNumberOfEntries" json:"total-entries,omitempty"`
+	EntriesInHealPending   *int       `xml:"numberOfEntriesInHealPending" json:"entries-in-heal-pending,omitempty"`
+	EntriesInSplitBrain    *int       `xml:"numberOfEntriesInSplitBrain" json:"entries-in-split-brain,omitempty"`
+	EntriesPossiblyHealing *int       `xml:"numberOfEntriesPossiblyHealing" json:"entries-possibly-healing,omitempty"`
+	Entries                *int       `xml:"numberOfEntries" json:"entries,omitempty"`
+	Files                  []FileGfID `xml:"file" json:"file-gfid,omitempty"`
 }
 
 // HealInfo represents structure of stdout while running glfsheal binary
