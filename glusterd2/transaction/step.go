d31eef4de5369554aa6756dcce9a48bbf1c002fd
Sridhar Seshasayee <sseshasa@redhat.com>
2018-07-03 01:37:52 +0530

    Use opencensus APIs to capture traces and spans for volume create handler.
    Use the context passed to the volume create handler and invoke the
    opencensus APIs to start tracing spans across the top level volume create
    transaction and all the steps involved for that transaction. Procedure
    involved,
      - Start a parent span using "trace.StartSpan()" at the beginning of
        the volumeCreateHandler().
      - Save the original context within the transaction config when a new
        transaction is created.
      - Pass the original context from the transaction config before
        invoking each step and use it to trace the span related to that step.
    
    The step name and the request ID pertaining to the transaction is
    saved into the name of the span which is shown in the tracing endpoint UI.
    
    Signed-off-by: Sridhar Seshasayee <sseshasa@redhat.com>

--- glusterd2/transaction/step.go
+++ glusterd2/transaction/step.go
@@ -1,6 +1,7 @@
 package transaction
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/pborman/uuid"
 	log "github.com/sirupsen/logrus"
+	"go.opencensus.io/trace"
 )
 
 // StepFunc is the function that is supposed to be run during a transaction step
@@ -33,14 +35,14 @@ var (
 )
 
 // do runs the DoFunc on the nodes
-func (s *Step) do(ctx TxnCtx) error {
-	return runStepFuncOnNodes(s.DoFunc, ctx, s.Nodes)
+func (s *Step) do(origCtx context.Context, ctx TxnCtx) error {
+	return runStepFuncOnNodes(origCtx, s.DoFunc, ctx, s.Nodes)
 }
 
 // undo runs the UndoFunc on the nodes
 func (s *Step) undo(ctx TxnCtx) error {
 	if s.UndoFunc != "" {
-		return runStepFuncOnNodes(s.UndoFunc, ctx, s.Nodes)
+		return runStepFuncOnNodes(nil, s.UndoFunc, ctx, s.Nodes)
 	}
 	return nil
 }
@@ -88,13 +90,13 @@ func (r stepResp) Status() int {
 	return http.StatusInternalServerError
 }
 
-func runStepFuncOnNodes(stepName string, ctx TxnCtx, nodes []uuid.UUID) error {
+func runStepFuncOnNodes(origCtx context.Context, stepName string, ctx TxnCtx, nodes []uuid.UUID) error {
 
 	respCh := make(chan stepPeerResp, len(nodes))
 	defer close(respCh)
 
 	for _, node := range nodes {
-		go runStepFuncOnNode(stepName, ctx, node, respCh)
+		go runStepFuncOnNode(origCtx, stepName, ctx, node, respCh)
 	}
 
 	// Ideally, we have to cancel the pending go-routines on first error
@@ -126,7 +128,7 @@ func runStepFuncOnNodes(stepName string, ctx TxnCtx, nodes []uuid.UUID) error {
 	return nil
 }
 
-func runStepFuncOnNode(stepName string, ctx TxnCtx, node uuid.UUID, respCh chan<- stepPeerResp) {
+func runStepFuncOnNode(origCtx context.Context, stepName string, ctx TxnCtx, node uuid.UUID, respCh chan<- stepPeerResp) {
 
 	ctx.Logger().WithFields(log.Fields{
 		"step": stepName, "node": node,
@@ -134,7 +136,7 @@ func runStepFuncOnNode(stepName string, ctx TxnCtx, node uuid.UUID, respCh chan<
 
 	var err error
 	if uuid.Equal(node, gdctx.MyUUID) {
-		err = runStepFuncLocally(stepName, ctx)
+		err = runStepFuncLocally(origCtx, stepName, ctx)
 	} else {
 		// remote node
 		err = runStepOn(stepName, node, ctx)
@@ -143,9 +145,16 @@ func runStepFuncOnNode(stepName string, ctx TxnCtx, node uuid.UUID, respCh chan<
 	respCh <- stepPeerResp{node, err}
 }
 
-func runStepFuncLocally(stepName string, ctx TxnCtx) error {
+func runStepFuncLocally(origCtx context.Context, stepName string, ctx TxnCtx) error {
 
 	var err error
+
+	if origCtx != nil {
+		reqID := ctx.GetTxnReqID()
+		spanName := stepName + " ReqID:" + reqID
+		_, span := trace.StartSpan(origCtx, spanName)
+		defer span.End()
+	}
 
 	stepFunc, ok := getStepFunc(stepName)
 	if ok {
