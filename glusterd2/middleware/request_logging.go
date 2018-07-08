a4d5742390592bea8224c3e97aef767940696328
John Mulligan <jmulligan@redhat.com>
2018-07-08 14:19:04 -0400

    middleware: log the incoming request prior to processing
    It is useful to log the request as it is being made to the
    server rather than relying entirely on logging generated
    after the request. Logging before the request is processed can
    help a person debugging with understanding when the client
    first made the request.
    Log the client's user agent string.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- glusterd2/middleware/request_logging.go
+++ glusterd2/middleware/request_logging.go
@@ -14,8 +14,16 @@ import (
 // Apache Common Log Format (CLF)
 func LogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		entry := log.WithField("reqid", gdctx.GetReqID(r.Context()).String())
+		entry := log.WithFields(log.Fields{
+			"subsys": "rest",
+			"reqid":  gdctx.GetReqID(r.Context()).String(),
+		})
+		entry.WithFields(log.Fields{
+			"method": r.Method,
+			"url":    r.URL,
+			"client": r.UserAgent(),
+		}).Info("HTTP Request")
 		delete(entry.Data, logging.SourceField)
-		handlers.LoggingHandler(entry.Writer(), next).ServeHTTP(w, r)
+		handlers.CombinedLoggingHandler(entry.Writer(), next).ServeHTTP(w, r)
 	})
 }
