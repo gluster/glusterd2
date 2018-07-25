83a8eb8a05bd7fce16805f88d60d0522db871cd3
Prashanth Pai <ppai@redhat.com>
2018-07-25 17:05:41 +0530

    volume-reset: Support resetting group options
    ...and do some minor cleanups
    
    Signed-off-by: Prashanth Pai <ppai@redhat.com>

--- glusterd2/commands/volumes/volume-reset.go
+++ glusterd2/commands/volumes/volume-reset.go
@@ -33,12 +33,19 @@ func volumeResetHandler(w http.ResponseWriter, r *http.Request) {
 
 	ctx := r.Context()
 	logger := gdctx.GetReqLogger(ctx)
-
-	volname := mux.Vars(r)["volname"]
+	var err error
 
 	var req api.VolOptionResetReq
 	if err := restutils.UnmarshalRequest(r, &req); err != nil {
 		restutils.SendHTTPError(ctx, w, http.StatusBadRequest, errors.ErrJSONParsingFailed)
+		return
+	}
+
+	volname := mux.Vars(r)["volname"]
+	volinfo, err := volume.GetVolume(volname)
+	if err != nil {
+		status, err := restutils.ErrToStatusCode(err)
+		restutils.SendHTTPError(ctx, w, status, err)
 		return
 	}
 
@@ -50,16 +57,18 @@ func volumeResetHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer txn.Done()
 
-	volinfo, err := volume.GetVolume(volname)
-	if err != nil {
-		status, err := restutils.ErrToStatusCode(err)
-		restutils.SendHTTPError(ctx, w, status, err)
+	// store volinfo to revert changes if transaction fails
+	oldvolinfo := volinfo
+	if err := txn.Ctx.Set("oldvolinfo", oldvolinfo); err != nil {
+		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, err)
 		return
 	}
-	//store volinfo to revert back changes in case of transaction failure
-	oldvolinfo := volinfo
-	// Delete the option after checking for volopt flags
-	opReset := false
+
+	req.Options, err = expandGroupOptionsReset(req.Options)
+	if err != nil {
+		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, err)
+		return
+	}
 
 	if req.All {
 		req.Options = []string{}
@@ -92,25 +101,18 @@ func volumeResetHandler(w http.ResponseWriter, r *http.Request) {
 			if op.IsForceRequired() {
 				if req.Force {
 					delete(volinfo.Options, k)
-					opReset = true
 				} else {
-					errMsg := "Option needs force flag to be set"
+					errMsg := "Option needs force flag to be reset"
 					restutils.SendHTTPError(ctx, w, http.StatusBadRequest, errMsg)
 					return
 				}
 			}
 			delete(volinfo.Options, k)
-			opReset = true
 		} else {
 			errMsg := "Option trying to reset is not set or invalid option"
 			restutils.SendHTTPError(ctx, w, http.StatusBadRequest, errMsg)
 			return
 		}
-	}
-	// Check if an option was reset, else return.
-	if !opReset {
-		restutils.SendHTTPResponse(ctx, w, http.StatusOK, volinfo.Options)
-		return
 	}
 
 	allNodes, err := peer.GetPeerIDs()
@@ -118,15 +120,13 @@ func volumeResetHandler(w http.ResponseWriter, r *http.Request) {
 		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, err)
 		return
 	}
-	//save volume information for transaction failure scenario
-	if err := txn.Ctx.Set("oldvolinfo", oldvolinfo); err != nil {
-		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, err)
-		return
-	}
+
+	// doing this to reuse isActionStepRequired() which takes a map
 	opt := make(map[string]string)
 	for _, key := range req.Options {
 		opt[key] = ""
 	}
+
 	txn.Steps = []*transaction.Step{
 		{
 			DoFunc:   "vol-option.XlatorActionDoReset",
@@ -144,13 +144,13 @@ func volumeResetHandler(w http.ResponseWriter, r *http.Request) {
 			Nodes:  allNodes,
 		},
 	}
+
 	if err := txn.Ctx.Set("req", &req); err != nil {
 		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, err)
 		return
 	}
 
 	if err := txn.Ctx.Set("volinfo", volinfo); err != nil {
-		logger.WithError(err).Error("failed to set volinfo in transaction context")
 		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, err)
 		return
 	}
@@ -162,5 +162,5 @@ func volumeResetHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	restutils.SendHTTPResponse(ctx, w, http.StatusOK, volinfo.Options)
+	restutils.SendHTTPResponse(ctx, w, http.StatusOK, volinfo)
 }
