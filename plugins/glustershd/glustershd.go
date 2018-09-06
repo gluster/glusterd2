0a5dee9b1dac7c3ed8c2f2809c0da6c5ef56a9d7
Aravinda VK <avishwan@redhat.com>
2018-09-06 21:20:55 +0530

    volgen: volgen changes for glustershd
    Fixes: #1111 and #1190
    Signed-off-by: Aravinda VK <avishwan@redhat.com>

--- plugins/glustershd/glustershd.go
+++ plugins/glustershd/glustershd.go
@@ -22,6 +22,7 @@ type Glustershd struct {
 	args           []string
 	socketfilepath string
 	pidfilepath    string
+	VolfileID      string
 }
 
 // Name returns human-friendly name of the glustershd process. This is used for
@@ -37,17 +38,17 @@ func (shd *Glustershd) Path() string {
 
 // Args returns arguments to be passed to glustershd process during spawn.
 func (shd *Glustershd) Args() []string {
-	shost, _, _ := net.SplitHostPort(config.GetString("clientaddress"))
+	shost, sport, _ := net.SplitHostPort(config.GetString("clientaddress"))
 	if shost == "" {
 		shost = "localhost"
 	}
-	volFileID := "gluster/glustershd"
 
 	logFile := path.Join(config.GetString("logdir"), "glusterfs", "glustershd.log")
 
 	shd.args = []string{}
 	shd.args = append(shd.args, "-s", shost)
-	shd.args = append(shd.args, "--volfile-id", volFileID)
+	shd.args = append(shd.args, "--volfile-server-port", sport)
+	shd.args = append(shd.args, "--volfile-id", shd.VolfileID)
 	shd.args = append(shd.args, "-p", shd.PidFile())
 	shd.args = append(shd.args, "-l", logFile)
 	shd.args = append(shd.args, "-S", shd.SocketFile())
@@ -87,7 +88,7 @@ func newGlustershd() (*Glustershd, error) {
 	if e != nil {
 		return nil, e
 	}
-	glustershdObject := &Glustershd{binarypath: path}
+	glustershdObject := &Glustershd{binarypath: path, VolfileID: "gluster/glustershd"}
 	return glustershdObject, nil
 }
 
