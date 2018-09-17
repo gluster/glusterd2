eca0fce842e174a2bc3f82479a98089d9acb6786
Madhu Rajanna <mrajanna@redhat.com>
2018-09-17 13:07:43 +0530

    remove code duplication in volume_ops_test
    call checkFuseAvailable from utils package
    to avoid code duplication
    
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

--- e2e/volume_ops_test.go
+++ e2e/volume_ops_test.go
@@ -354,9 +354,7 @@ func testVolumeInfo(t *testing.T) {
 }
 
 func testVolumeStatus(t *testing.T) {
-	if _, err := os.Lstat("/dev/fuse"); os.IsNotExist(err) {
-		t.Skip("skipping mount /dev/fuse unavailable")
-	}
+	checkFuseAvailable(t)
 	r := require.New(t)
 
 	_, err := client.VolumeStatus(volname)
@@ -406,9 +404,7 @@ func testVolumeMount(t *testing.T, tc *testCluster) {
 }
 
 func testMountUnmount(t *testing.T, v string, tc *testCluster) {
-	if _, err := os.Lstat("/dev/fuse"); os.IsNotExist(err) {
-		t.Skip("skipping mount /dev/fuse unavailable")
-	}
+	checkFuseAvailable(t)
 	r := require.New(t)
 
 	mntPath := testTempDir(t, "mnt")
