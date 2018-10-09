7813bf31279d54c8b3893aa4991a05157460eb26
Prashanth Pai <ppai@redhat.com>
2018-10-09 12:22:26 +0530

    Refactor cluster options package
    * Merge 'glusterd2/cluster' package into `glusterd2/options` package.
    * Rename 'glusterd2/commands/global' to 'glusterd2/commands/options'.
    
    Signed-off-by: Prashanth Pai <ppai@redhat.com>

--- glusterd2/commands/options/setclusteroptions.go
+++ glusterd2/commands/options/setclusteroptions.go
@@ -1,47 +1,48 @@
-package globalcommands
+package optionscommands
 
 import (
 	"fmt"
 	"net/http"
 
-	"github.com/gluster/glusterd2/glusterd2/cluster"
+	"github.com/gluster/glusterd2/glusterd2/options"
 	restutils "github.com/gluster/glusterd2/glusterd2/servers/rest/utils"
 	"github.com/gluster/glusterd2/pkg/api"
 	"github.com/gluster/glusterd2/pkg/errors"
 )
 
-func setGlobalOptionsHandler(w http.ResponseWriter, r *http.Request) {
+func setClusterOptionsHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	var req api.GlobalOptionReq
+	var req api.ClusterOptionReq
 	if err := restutils.UnmarshalRequest(r, &req); err != nil {
 		restutils.SendHTTPError(ctx, w, http.StatusBadRequest, errors.ErrJSONParsingFailed)
 		return
 	}
 
-	c, err := cluster.GetCluster()
-	// ErrClusterNotFound here implies that no global option has yet been explicitly set. Ignoring it.
-	if err != nil && err != errors.ErrClusterNotFound {
-		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, fmt.Sprintf("Problem retrieving cluster information from etcd store: %s", err.Error()))
+	// TODO: Take lock here
+
+	c, err := options.GetClusterOptions()
+	if err != nil && err != errors.ErrClusterOptionsNotFound {
+		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, err)
 		return
 	}
 
 	for k, v := range req.Options {
-		if _, found := cluster.GlobalOptMap[k]; found {
+		if _, found := options.ClusterOptMap[k]; found {
 			// TODO validate the value type for global option
 
 			if c == nil {
-				c = new(cluster.Cluster)
+				c = new(options.ClusterOptions)
 				c.Options = make(map[string]string)
 			}
 			c.Options[k] = v
 		} else {
 			restutils.SendHTTPError(ctx, w, http.StatusInternalServerError, fmt.Sprintf("Invalid global option: %s", k))
-			continue
+			return
 		}
 	}
 
-	if err := cluster.UpdateCluster(c); err != nil {
+	if err := options.UpdateClusterOptions(c); err != nil {
 		restutils.SendHTTPError(ctx, w, http.StatusInternalServerError,
 			fmt.Sprintf("Failed to update store with cluster attributes %s", err.Error()))
 		return
