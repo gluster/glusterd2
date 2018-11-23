419ae7dff5aeb0c7b5e07c37b72ad500811f41c5
Aravinda VK <avishwan@redhat.com>
2018-11-23 18:23:46 +0530

    Fix the Volume Capacity display in CLI
    Earlier `humanReadable` function was accepting values in `MiB`
    With PR #1326 default unit is changed to bytes.
    
    Signed-off-by: Aravinda VK <avishwan@redhat.com>

--- glustercli/cmd/utils_test.go
+++ glustercli/cmd/utils_test.go
@@ -3,14 +3,22 @@ package cmd
 import (
 	"testing"
 
+	gutils "github.com/gluster/glusterd2/pkg/utils"
+
 	"github.com/stretchr/testify/assert"
 )
 
 // TestHumanReadable checks if the function returns the right size with unit
-// for the input given in MB
+// for the input given in bytes
 func TestHumanReadable(t *testing.T) {
-	assert.Equal(t, "20.0 MB", humanReadable(20))
-	assert.Equal(t, "1.0 GB", humanReadable(1024))
-	assert.Equal(t, "1.5 GB", humanReadable(1536))
-	assert.Equal(t, "1.0 TB", humanReadable(1048576))
+	assert.Equal(t, "900.0 B", humanReadable(900))
+	assert.Equal(t, "1.0 KiB", humanReadable(1024))
+	assert.Equal(t, "1.5 KiB", humanReadable(1536))
+	assert.Equal(t, "1.0 MiB", humanReadable(1*gutils.MiB))
+	assert.Equal(t, "1.5 MiB", humanReadable(1.5*gutils.MiB))
+	assert.Equal(t, "20.0 KiB", humanReadable(20*gutils.KiB))
+	assert.Equal(t, "20.0 MiB", humanReadable(20*gutils.MiB))
+	assert.Equal(t, "1.0 GiB", humanReadable(1*gutils.GiB))
+	assert.Equal(t, "1.5 GiB", humanReadable(1536*gutils.MiB))
+	assert.Equal(t, "1.0 TiB", humanReadable(1*gutils.TiB))
 }
