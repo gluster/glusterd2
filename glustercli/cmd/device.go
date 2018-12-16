18c0b006be0f319b70c90b1430f801d928bdd79b
Madhu Rajanna <mrajanna@redhat.com>
2018-12-16 15:04:30 +0530

    Add device info cli command
    * Added device info CLI command, which
    will list the devices if the Peer ID is
    passed or else it will list all devices
    in the cluster.
    
    ```
    [root@dhcp43-209 build]# ./glustercli device info
    
    Peer Name: dhcp43-209.lab.eng.blr.redhat.com
    Peer ID: 335f3b47-cdc6-46ad-806e-6340f867c428
    +----------+---------+------------+-----------+-----------+--------+
    |  DEVICE  |  STATE  | TOTAL SIZE | FREE SIZE | USED SIZE | USED % |
    +----------+---------+------------+-----------+-----------+--------+
    | /dev/vdb | enabled | 0.0 B      | 0.0 B     | 0.0 B     |   0.00 |
    | /dev/vdc | enabled | 199.9 GiB  | 129.1 GiB | 70.8 GiB  |  35.41 |
    +----------+---------+------------+-----------+-----------+--------+
    
    Peer Name: dhcp43-106.lab.eng.blr.redhat.com
    Peer ID: 7d0df840-698a-4604-acf0-457fe87e4641
    +----------+---------+------------+-----------+-----------+--------+
    |  DEVICE  |  STATE  | TOTAL SIZE | FREE SIZE | USED SIZE | USED % |
    +----------+---------+------------+-----------+-----------+--------+
    | /dev/vdb | enabled | 199.9 GiB  | 149.2 GiB | 50.7 GiB  |  25.35 |
    +----------+---------+------------+-----------+-----------+--------+
    
    Peer Name: dhcp42-71.lab.eng.blr.redhat.com
    Peer ID: e0567090-202a-429c-a13e-408a5f36e312
    +----------+---------+------------+-----------+-----------+--------+
    |  DEVICE  |  STATE  | TOTAL SIZE | FREE SIZE | USED SIZE | USED % |
    +----------+---------+------------+-----------+-----------+--------+
    | /dev/vdb | enabled | 199.9 GiB  | 149.2 GiB | 50.7 GiB  |  25.35 |
    +----------+---------+------------+-----------+-----------+--------+
    
    [root@dhcp43-209 build]# ./glustercli device info e0567090-202a-429c-a13e-408a5f36e312
    
    Peer Name: dhcp42-71.lab.eng.blr.redhat.com
    Peer ID: e0567090-202a-429c-a13e-408a5f36e312
    +----------+---------+------------+-----------+-----------+--------+
    |  DEVICE  |  STATE  | TOTAL SIZE | FREE SIZE | USED SIZE | USED % |
    +----------+---------+------------+-----------+-----------+--------+
    | /dev/vdb | enabled | 199.9 GiB  | 149.2 GiB | 50.7 GiB  |  25.35 |
    +----------+---------+------------+-----------+-----------+--------+
    
    ```
    
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

--- glustercli/cmd/device.go
+++ glustercli/cmd/device.go
@@ -2,23 +2,105 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/gluster/glusterd2/plugins/device/api"
+
+	"github.com/olekukonko/tablewriter"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 const (
-	helpDeviceCmd    = "Gluster Devices Management"
-	helpDeviceAddCmd = "add device"
+	helpDeviceCmd     = "Gluster Devices Management"
+	helpDeviceAddCmd  = "Add device"
+	helpDeviceInfoCmd = "Get device info"
 )
 
 func init() {
 	deviceCmd.AddCommand(deviceAddCmd)
+	deviceCmd.AddCommand(deviceInfoCmd)
 }
 
 var deviceCmd = &cobra.Command{
 	Use:   "device",
 	Short: helpDeviceCmd,
+}
+
+var deviceInfoCmd = &cobra.Command{
+	Use:   "info <PeerID>",
+	Short: helpDeviceInfoCmd,
+	Args:  cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var peerID string
+		if len(args) == 1 {
+			peerID = args[0]
+		}
+
+		if peerID == "" {
+			peers, err := client.Peers()
+			if err != nil {
+				if GlobalFlag.Verbose {
+					log.WithError(err).Error("peer list failed")
+				}
+				failure("Failed to get peer list", err, 1)
+			}
+			for _, peer := range peers {
+				peerID := peer.ID.String()
+				deviceList, err := client.DeviceList(peerID, "")
+				if err != nil {
+					if GlobalFlag.Verbose {
+						log.WithError(err).Error("device list failed")
+					}
+					failure("Failed to get device list", err, 1)
+				}
+				if len(deviceList) == 0 {
+					continue
+				}
+				deviceListDisplay(peerID, peer.Name, deviceList)
+			}
+			return
+		}
+
+		peerInfo, err := client.GetPeer(peerID)
+		if err != nil {
+			if GlobalFlag.Verbose {
+				log.WithError(err).Error("peer list failed")
+			}
+			failure("Failed to get peer list", err, 1)
+		}
+
+		deviceList, err := client.DeviceList(peerID, "")
+		if err != nil {
+			if GlobalFlag.Verbose {
+				log.WithError(err).WithField("peerid", peerID).Error("device list failed")
+			}
+			failure("Device list failed", err, 1)
+		}
+		if len(deviceList) == 0 {
+			fmt.Println("No devices are associated with given peer")
+			return
+		}
+		deviceListDisplay(peerID, peerInfo.Name, deviceList)
+	},
+}
+
+func deviceListDisplay(peerID, peerName string, deviceList []api.Info) {
+	fmt.Println()
+	fmt.Println("Peer Name:", peerName)
+	fmt.Println("Peer ID:", peerID)
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Device", "State", "Total Size", "Free Size", "Used Size", "Used %"})
+	for _, d := range deviceList {
+		var usedPer float64
+		if d.UsedSize > 0 {
+			usedPer = float64(d.UsedSize) / float64(d.TotalSize) * 100
+		}
+
+		table.Append([]string{d.Device, d.State, humanReadable(d.TotalSize),
+			humanReadable(d.AvailableSize), humanReadable(d.UsedSize), fmt.Sprintf("%.2f", usedPer)})
+	}
+	table.Render()
 }
 
 var deviceAddCmd = &cobra.Command{
