a7822bb71562062e00c1790f7f1845f7ad9867c9
Oshank Kumar <okumar@redhat.com>
2019-01-02 19:25:45 +0530

    Migrate volume operations(start,stop,del) to new txn framework
    Signed-off-by: Oshank Kumar <okumar@redhat.com>

--- glusterd2/commands/volumes/volume-stop.go
+++ glusterd2/commands/volumes/volume-stop.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gluster/glusterd2/glusterd2/gdctx"
 	restutils "github.com/gluster/glusterd2/glusterd2/servers/rest/utils"
 	"github.com/gluster/glusterd2/glusterd2/transaction"
+	transactionv2 "github.com/gluster/glusterd2/glusterd2/transactionv2"
 	"github.com/gluster/glusterd2/glusterd2/volgen"
 	"github.com/gluster/glusterd2/glusterd2/volume"
 	"github.com/gluster/glusterd2/pkg/api"
@@ -123,7 +124,7 @@ func volumeStopHandler(w http.ResponseWriter, r *http.Request) {
 	logger := gdctx.GetReqLogger(ctx)
 	volname := mux.Vars(r)["volname"]
 
-	txn, err := transaction.NewTxnWithLocks(ctx, volname)
+	txn, err := transactionv2.NewTxnWithLocks(ctx, volname)
 	if err != nil {
 		status, err := restutils.ErrToStatusCode(err)
 		restutils.SendHTTPError(ctx, w, status, err)
