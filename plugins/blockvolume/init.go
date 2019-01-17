384d8bb87e3190df38c50eff71c2f865be9c3d16
Kotresh HR <khiremat@redhat.com>
2019-01-17 15:52:58 +0530

    gluster-virtblock: Rename to virtual block
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- plugins/blockvolume/init.go
+++ plugins/blockvolume/init.go
@@ -3,5 +3,5 @@ package blockvolume
 import (
 	// initialise all block providers
 	_ "github.com/gluster/glusterd2/plugins/blockvolume/blockprovider/gluster-block"
-	_ "github.com/gluster/glusterd2/plugins/blockvolume/blockprovider/gluster-loopback"
+	_ "github.com/gluster/glusterd2/plugins/blockvolume/blockprovider/gluster-virtblock"
 )
